7e227c53f77703523daa31138c89e63f329afe60
Suraj1131 <surajkokane11@gmail.com>
2024-02-18 17:00:57 +0530

    second commit

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,9 +7,18 @@ import { AppService } from '../app.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+[x: string]: any;
   users: any;
   firstname: any;
   lastname: any;
+  email: any;
+  phoneNumber: any;
+  age: any;
+  state: any;
+  country: any;
+  address: any;
+  tags: any;
+  newsletter: any;
 
   constructor(private AppService:AppService){
 
@@ -26,6 +35,14 @@ export class ProfileComponent {
         this.users=result.users;
         this.firstname=this.users[0].firstName
         this.lastname=this.users[0].lastName
+        this.email=this.users[0].email
+        this.phoneNumber=this.users[0].phoneNumber
+        this.age=this.users[0].age
+        this.state=this.users[0].state
+        this.country=this.users[0].country
+        this.address=this.users[0].address
+        this.tags=this.users[0].tags
+        this.newsletter=this.users[0].newsletter
         console.log(this.users[0].firstName);
         
         
